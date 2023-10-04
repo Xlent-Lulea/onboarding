189b8541a70731be29f1ae0fefeaee2048a13277
sohjla <sofia.hjort.labba@gmail.com>
2023-10-04 10:24:27 +0200

    Update sql script

--- src/main/resources/dbscript.sql
+++ src/main/resources/dbscript.sql
@@ -1,21 +1,24 @@
 -- Skapa upp en databas som går att använda för att testa applikationen.
-
-CREATE TABLE TASK (
-  id BIGSERIAL PRIMARY KEY,
-  tasktype VARCHAR(255),
-  urltitle VARCHAR(255),
-  description VARCHAR(255),
-  url VARCHAR(255),
-  completed BOOLEAN,
-  person_id BIGINT
-);
-
-
 CREATE TABLE PERSON (
   id BIGSERIAL PRIMARY KEY,
   name VARCHAR(255),
   email VARCHAR(255),
-  active BOOLEAN
+  is_active BOOLEAN
+);
+
+CREATE TABLE TASK-ENTITY (
+  id BIGSERIAL PRIMARY KEY,
+  task_id BIGINT REFERENCES TASK(id),
+  person_id BIGINT REFERENCES PERSON(id),
+  is_completed BOOLEAN
+);
+
+CREATE TABLE TASK (
+  id BIGSERIAL PRIMARY KEY,
+  type VARCHAR(255),
+  title VARCHAR(255),
+  description VARCHAR(255),
+  url VARCHAR(255)
 );
 
 -- Lägg till person i Postman via POST 127.0.0.1:8081/createPerson och få standardtask (PersonController.java)
